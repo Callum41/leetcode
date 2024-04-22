17c044eaa0ff9cd6d850e27fb3bdeca90fed9257
Juan Gamo <juan.sgamo@alumnos.upm.es>
2024-04-22 20:53:01 +0200

    Added the first exercise

---
+++ 1. Two Sum/Solution.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] sol = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                sol[0] = map.get(complement);
+                sol[1] = i;
+                return sol;
+            }
+            else map.put(nums[i], i);
+        }
+        return sol;
+    }
+}
